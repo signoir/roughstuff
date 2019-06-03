56e628b9960a86e601d57feba1a13ad8582f4eff
anonymoussc <50c5ac69@opayq.com>
2019-06-03 14:02:24 +0700

    feat(entities): new ./app/entities/items.ts

---
+++ src/app/entities/items.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 6/2/19 1:41 PM
+ */
+
+/*
+ * items.ts
+ * Created by @anonymoussc on 06/02/2019 1:40 PM.
+ */
+
+import { Item } from './Item';
+
+export type Items = Item[];
