49b1a779a21dda033aa70029adc6ad8e137786a8
anonymoussc <50c5ac69@opayq.com>
2019-06-03 14:01:05 +0700

    feat(components): new components.module

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 6/3/19 2:01 PM
+ */
+
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  schemas     : [CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [],
+  imports     : [
+    CommonModule
+  ]
+})
+export class ComponentsModule {}
