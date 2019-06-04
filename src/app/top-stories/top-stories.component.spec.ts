ac9ffc0ae669aff55e0a5cd5618b8f2f306e8891
anonymoussc <50c5ac69@opayq.com>
2019-06-04 15:31:50 +0700

    refactor(top-stories): modified src/app/top-stories/top-stories.component.spec

--- src/app/top-stories/top-stories.component.spec.ts
+++ src/app/top-stories/top-stories.component.spec.ts
@@ -5,33 +5,40 @@
 
 /*
  * Copyright(c) 2019. All rights reserved.
- * Last modified 6/3/19 4:54 PM
+ * Last modified 6/4/19 3:31 PM
  */
 
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { TestUtils } from '../../testing/test-utils';
 import { TopStoriesComponent } from './top-stories.component';
+import { ItemsComponent } from '../components/items/items.component';
+import { ItemComponent } from '../components/item/item.component';
+import { TimeAgoPipe } from '../components/time-ago/time-ago.pipe';
+import { ItemService } from '../services/item/item.service';
+import { ItemServiceMock } from '../services/item/item.service.mock';
 
-describe('TopStoriesComponent', () => {
-  let component: TopStoriesComponent;
-  let fixture: ComponentFixture<TopStoriesComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [TopStoriesComponent],
-      schemas     : [CUSTOM_ELEMENTS_SCHEMA],
-    })
-      .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture   = TestBed.createComponent(TopStoriesComponent);
-    component = fixture.componentInstance;
+let fixture: ComponentFixture<TopStoriesComponent> = null;
+let component: any                                 = null;
+describe('top stories page', () => {
+  beforeEach(async(() => TestUtils.beforeEachCompiler(
+    [
+      TopStoriesComponent, ItemsComponent, ItemComponent,
+      TimeAgoPipe
+    ],
+    [{provide: ItemService, useClass: ItemServiceMock}]
+  ).then(compiled => {
+    fixture   = compiled.fixture;
+    component = compiled.instance;
+  })));
+  it('should display a list of 10 items', async(() => {
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      const debugElements = fixture.debugElement.queryAll(By.css('h2'));
+      expect(debugElements.length).toBe(10);
+      expect(debugElements[0].nativeElement.textContent).toContain('Item 1');
+      expect(debugElements[1].nativeElement.textContent).toContain('Item 2');
+    });
+  }));
 });
