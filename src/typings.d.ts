8b9b023207685419e10f99e866131faf52f71a23
anonymoussc <50c5ac69@opayq.com>
2019-06-07 06:51:48 +0700

    feat(app): new ./typings.d.ts

---
+++ src/typings.d.ts
@@ -0,0 +1,12 @@
+/* SystemJS module definition */
+
+/*
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 6/7/19 6:51 AM
+ */
+
+declare var module: NodeModule;
+
+interface NodeModule {
+  id: string;
+}
