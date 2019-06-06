1074ad3cc1ea093b65bf1be9a4994e9c2d95efba
anonymoussc <50c5ac69@opayq.com>
2019-06-07 06:32:22 +0700

    feat(app): new ./app/app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,81 @@
 /*
  * Copyright(c) 2019. All rights reserved.
- * Last modified 6/3/19 2:27 PM
+ * Last modified 6/7/19 6:32 AM
  */
 
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, ErrorHandler, NgModule, NgZone, PLATFORM_ID } from '@angular/core';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { RouteReuseStrategy } from '@angular/router';
-import { ComponentsModule } from './components/components.module';
+
+import { MyAppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { ServicesModule } from './services/services.module';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabase, AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { TimeAgoPipe } from './components/time-ago/time-ago.pipe';
+import { CustomRouterStateSerializer, reducers } from './reducers';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { ItemsEffects } from './effects/items';
+import { AuthModule } from './auth/auth.module';
+import { HACKER_NEWS_DB } from './hackernews-db';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FavoritesModule } from './favorites/favorites.module';
+import { CustomErrorHandler } from './error-handler';
 
 @NgModule({
-  declarations   : [
-    AppComponent,
-    TimeAgoPipe
+  declarations: [
+    MyAppComponent,
   ],
-  entryComponents: [],
-  imports        : [
-    BrowserModule,
-    IonicModule.forRoot(),
+  imports     : [
     AppRoutingModule,
-    ComponentsModule,
-    ServicesModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.app_db),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    IonicModule.forRoot(),
+    ServicesModule,
+    AuthModule,
+    FavoritesModule,
+    StoreModule.forRoot(reducers),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router',
+    }),
+    StoreDevtoolsModule.instrument({
+      name   : 'NgRx HNC DevTools',
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([ItemsEffects]),
   ],
-  providers      : [
+  bootstrap   : [MyAppComponent],
+  schemas     : [CUSTOM_ELEMENTS_SCHEMA],
+  providers   : [
+    {provide: RouterStateSerializer, useClass: CustomRouterStateSerializer},
+    {
+      provide   : HACKER_NEWS_DB,
+      useFactory: (platformId: Object, zone: NgZone) =>
+        new AngularFireDatabase(environment.hackernews_db, 'HackerNews', null, platformId, zone),
+      deps      : [PLATFORM_ID, NgZone]
+    },
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    {provide: ErrorHandler, useClass: CustomErrorHandler},
     StatusBar,
     SplashScreen,
-    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
-  ],
-  bootstrap      : [AppComponent]
+    InAppBrowser,
+    SocialSharing,
+  ]
 })
-export class AppModule {}
+export class AppModule {
+}
