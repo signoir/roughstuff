f03ce4b5bd8d1c9d6fea0a6e7266c58f3198e2ac
anonymoussc <50c5ac69@opayq.com>
2019-06-07 06:38:50 +0700

    feat(app): update ./environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 6/7/19 6:38 AM
+ */
+
 export const environment = {
-  production: true
+  production   : true,
+  hackernews_db: {
+    databaseURL: 'https://hacker-news.firebaseio.com',
+  },
+  app_db       : {
+    apiKey           : 'AIzaSyAonue_QoCXiE1w2_bK9htgaqaqSwAcNK0',
+    authDomain       : 'hacker-news-client-v4.firebaseapp.com',
+    databaseURL      : 'https://hacker-news-client-v4.firebaseio.com',
+    projectId        : 'hacker-news-client-v4',
+    storageBucket    : 'hacker-news-client-v4.appspot.com',
+    messagingSenderId: '235153545323',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,24 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false
-};
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 
 /*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 6/7/19 6:38 AM
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+export const environment = {
+  production   : false,
+  hackernews_db: {
+    databaseURL: 'https://hacker-news.firebaseio.com',
+  },
+  app_db       : {
+    apiKey           : 'AIzaSyAonue_QoCXiE1w2_bK9htgaqaqSwAcNK0',
+    authDomain       : 'hacker-news-client-v4.firebaseapp.com',
+    databaseURL      : 'https://hacker-news-client-v4.firebaseio.com',
+    projectId        : 'hacker-news-client-v4',
+    storageBucket    : 'hacker-news-client-v4.appspot.com',
+    messagingSenderId: '235153545323',
+  },
+};
