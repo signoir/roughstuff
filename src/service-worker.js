ee3e8fdebfea568de8a2d9c83257495348762f61
anonymoussc <50c5ac69@opayq.com>
2019-06-07 06:49:02 +0700

    feat(app): new ./service-worker.js

---
+++ src/service-worker.js
@@ -0,0 +1,36 @@
+/**
+ * Check out https://googlechromelabs.github.io/sw-toolbox/ for
+ * more info on how to use sw-toolbox to custom configure your service worker.
+ */
+
+
+/*
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 5/29/19 9:35 AM
+ */
+
+'use strict';
+importScripts('./build/sw-toolbox.js');
+
+self.toolbox.options.cache = {
+  name: 'ionic-cache'
+};
+
+// pre-cache our key assets
+self.toolbox.precache(
+  [
+    './build/main.js',
+    './build/vendor.js',
+    './build/main.css',
+    './build/polyfills.js',
+    'index.html',
+    'manifest.json'
+  ]
+);
+
+// dynamically cache any other local assets
+self.toolbox.router.any('/*', self.toolbox.fastest);
+
+// for any other requests go to the network, cache,
+// and then only use that cached resource if your user goes offline
+self.toolbox.router.default = self.toolbox.networkFirst;
