00878a9b04ae24303e6add4b0571af6d79362ae1
anonymoussc <50c5ac69@opayq.com>
2019-06-07 06:31:30 +0700

    feat(app): modified ./app/app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 6/7/19 6:31 AM
+ */
+
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
@@ -5,10 +10,10 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+  selector   : 'app-root',
+  templateUrl: 'app.html'
 })
-export class AppComponent {
+export class MyAppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
